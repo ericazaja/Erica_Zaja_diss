bf5d9758c936d977ba184f061a37974d2e317ae3
Erica Zaja <ericazaja@gmail.com>
2021-10-22 20:13:01 +0100

    E tidies messy folder

--- scripts/phenology_experiments.R
+++ scripts/phenology_experiments.R
@@ -11,3 +11,6 @@ library(readr)
 # Loading data -----
 phenology_data <- read_csv("datasets/phenology_data/CCIN13215_20210302_tundra_phenology_database.csv")
 
+# Data wrangling -----
+# Keep Toolik lake and Qikiqtaruk (close enough to PCH summer range) 
+# and perhaps Atqasuk, Utqiaġvik that are on the North slope of Alaska
