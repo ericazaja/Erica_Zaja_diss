ba112b50731f2da34d3c329de72b41ac93343e96
Erica Zaja <ericazaja@gmail.com>
2021-10-22 20:01:04 +0100

    E adds phenology data and start of script

---
+++ scripts/phenology_experiments.R
@@ -0,0 +1,13 @@
+##%######################################################%##
+#                                                          #
+####         PHENOLOGY  EXPERIMENTS -----               ####
+#               Erica Zaja - 22/10/2021                   ##
+#                                                         #
+##%######################################################%##
+
+# Loading libraries -----
+library(readr)
+
+# Loading data -----
+phenology_data <- read_csv("datasets/phenology_data/CCIN13215_20210302_tundra_phenology_database.csv")
+
