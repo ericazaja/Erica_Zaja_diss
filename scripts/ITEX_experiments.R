0116f35bff603589123c80c32e680647ed32436a
Erica Zaja <ericazaja@gmail.com>
2021-10-22 20:18:10 +0100

    E adds mariana's itex data and start of script

---
+++ scripts/ITEX_experiments.R
@@ -0,0 +1,10 @@
+##%######################################################%##
+#                                                          #
+####         ITEX VEG COVER EXPERIMENTS -----             ###
+#               Erica Zaja - 22/10/2021                   ##
+#                                                         #
+##%######################################################%##
+
+# Loading data ----
+load("~/Desktop/dissertation/R_dissertation/datasets/ITEX_data/ITEX_ALL.RData")
+load("~/Desktop/dissertation/R_dissertation/datasets/ITEX_data/full_itex_marianas_version_sept21.RData")
