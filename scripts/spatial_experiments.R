941e58a91542ad1ac1824b1bd60ec44787d209d7
Erica Zaja <ericazaja@gmail.com>
2022-01-28 13:21:26 +0000

    E tries overlaying PCH core range with shrub map

--- scripts/spatial_experiments.R
+++ scripts/spatial_experiments.R
@@ -12,45 +12,26 @@ library(raster)
 library(ggplot2)
 library(viridis)
 library(rasterVis)
+library(sf)
+library(tidyverse)
 
 # Loading data -----
-berner_dataset <- read_csv("datasets/berner_data/data/berner_dataset.csv")
-plant_agb_p2_5 <- raster("datasets/berner_data/data/plant_agb_p2_5.tif")
-# class      : RasterLayer 
-# dimensions : 14890, 35405, 527180450  (nrow, ncol, ncell)
-# resolution : 30, 30  (x, y)
-# extent     : -540475.3, 521674.7, 1933928, 2380628  (xmin, xmax, ymin, ymax)
-# crs        : +proj=aea +lat_0=50 +lon_0=-154 +lat_1=55 +lat_2=65 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs 
-# source     : /Users/ericazaja/Desktop/dissertation/R_dissertation/datasets/berner_data/data/plant_agb_p2_5.tif 
-# names      : plant_agb_p2_5 
-# values     : 0, 65535  (min, max)
-
-plant_agb_p50 <- raster("datasets/berner_data/data/plant_agb_p50.tif")
+berner_dataset <- read_csv("datasets/berner_data/berner_dataset.csv")
+shrub_agb_p2_5 <- raster("datasets/berner_data/shrub_agb_p2_5.tif")
+plant_agb_p50 <- raster("datasets/berner_data/plant_agb_p50.tif")
 
 # coordinate reference system (CRS) 
 
-shrub_agb_p2_5 <- raster("datasets/berner_data/data/shrub_agb_p2_5.tif")
-# class      : RasterLayer 
-# dimensions : 14890, 35405, 527180450  (nrow, ncol, ncell)
-# resolution : 30, 30  (x, y)
-# extent     : -540475.3, 521674.7, 1933928, 2380628  (xmin, xmax, ymin, ymax)
-# crs        : +proj=aea +lat_0=50 +lon_0=-154 +lat_1=55 +lat_2=65 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs 
-# source     : /Users/ericazaja/Desktop/dissertation/R_dissertation/datasets/berner_data/data/shrub_agb_p2_5.tif 
-# names      : shrub_agb_p2_5 
-# values     : 0, 65535  (min, max)
-
-shrub_dominance_of_agb_p50 <- raster("datasets/berner_data/data/shrub_dominance_of_agb_p50.tif")
-
+shrub_dominance_of_agb_p50 <- raster("datasets/berner_data/shrub_dominance_of_agb_p50.tif")
 
 ### N.B.same plot=same values in raster info
 
 # Comparing rasters to see if they have the same extent, number of rows and column, projection, resolution and origin
-compareRaster(shrub_agb_p2_5, plant_agb_p2_5)
+compareRaster(shrub_agb_p2_5, plant_agb_p50)
 # TRUE
 
 # Plotting rasters -----
 plot(shrub_agb_p2_5)
-plot(plant_agb_p2_5)
 plot(plant_agb_p50)
 plot(shrub_dominance_of_agb_p50)
 dev.off()
@@ -78,7 +59,7 @@ gplot_shrub_agb_p2_5 <- gplot(shrub_agb_p2_5) +
 # ggsave("gplot_shrub_agb_p2_5", scale = 1.5, dpi = 300) 		# to save plot
 
 # Plant cover p2_5
-gplot_plant_agb_p2_5 <- gplot(plant_agb_p2_5) +
+gplot_plant_agb_p2_5 <- gplot(plant_agb_p50) +
   geom_raster(aes(x = x, y = y, fill = value)) +
   # value is the specific value (of reflectance) each pixel is associated with
   scale_fill_viridis_c() +
@@ -115,13 +96,6 @@ gplot(t) +
 # https://gis.stackexchange.com/questions/34310/opening-lyr-file-via-rgdal-ogr
 range <- readOGR("datasets/PCH_data/Summer_range/v10/cyr_2013.gdb")
 plot(range)
-# class       : SpatialPolygonsDataFrame 
-# features    : 1 
-# extent      : 295021.4, 722245.6, 2015442, 2271115  (xmin, xmax, ymin, ymax)
-# crs         : +proj=aea +lat_0=50 +lon_0=-154 +lat_1=55 +lat_2=65 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs 
-# variables   : 7
-# names       : SEASONAL_, SEASONAL_I,      HERD,        RANGE,    Shape_Leng,     Shape_Length,       Shape_Area 
-# value       :        29,        122, Porcupine, Summer Range, 1511342.57305, 1568629.41263943, 73550115378.1924 
 
 ### HOW DO I OVERLAY A RASTER layer WITH A spatial POLYGON? -----
 # https://rspatial.org/raster/rosu/Chapter11.html 
@@ -130,7 +104,7 @@ library(rspatial)
 library(maptools)
 library(rgeos)
 
-projection(plant_agb_p2_5)
+projection(shrub_agb_p2_5)
 projection(range)
 # same projection
 
@@ -145,3 +119,27 @@ cropped <- crop(plant_agb_p2_5, range)
 masked <- mask(cropped, range)
 plot(masked)
 plot(range, add=T)
+
+
+#### PCH core range ----
+PCH_core_range <- st_read("datasets/PCH_Core_Range_2016/PCH_Core_Range_2016.shp") #loading data
+projection(PCH_core_range)
+projection(shrub_agb_p2_5)
+# same projection
+
+ggplot() + 
+  geom_sf(data = PCH_core_range, size = 0.5, color = "black", fill = "green") + 
+  ggtitle("PCH core range 2016") + 
+  coord_sf()
+
+PCH_core_range <- spTransform(PCH_core_range, crs(shrub_agb_p2_5)) # doesnt work
+
+plot(shrub_agb_p2_5)
+plot(PCH_core_range, add = TRUE)
+e <- extract(shrub_agb_p2_5, PCH_core_range)
+
+cropped <- crop(shrub_agb_p2_5, PCH_core_range)
+plot(cropped)
+
+
+
