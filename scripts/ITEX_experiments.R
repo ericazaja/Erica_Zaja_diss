7001c53f4a9f2593c08ae953331e021cb0e2c0e5
Erica Zaja <ericazaja@gmail.com>
2022-01-29 16:45:04 +0000

    E plots veg cover over time

--- scripts/ITEX_experiments.R
+++ scripts/ITEX_experiments.R
@@ -5,6 +5,9 @@
 #                                                         #
 ##%######################################################%##
 
+# Loading libraries ----
+library(tidyverse)
+
 # Loading data ----
 load("~/Desktop/dissertation/R_dissertation/datasets/ITEX_data/ITEX_ALL.RData")
 load("~/Desktop/dissertation/R_dissertation/datasets/ITEX_data/full_itex_marianas_version_sept21.RData")
@@ -16,16 +19,48 @@ load("~/Desktop/dissertation/R_dissertation/datasets/ITEX_data/full_itex_mariana
 ## Data exploration
 max(itex.full11$YEAR) # latest year
 min(itex.full11$YEAR) # earliest year
-unique(itex.full11$SiteSubsite) # Unique site names
+unique(itex.full11$SITE) # Unique site names
 
-## eg code to clean out species from https://ourcodingclub.github.io/tutorials/model-design/ 
-# toolik_plants <- toolik_plants %>%
-#  filter(!Species %in% c("Woody cover", "Tube",
-#                         "Hole", "Vole trail",
-#                         "removed", "vole turds",
-#                         "Mushrooms", "Water",
-#                        "Caribou poop", "Rocks",
-#                         "mushroom", "caribou poop",
-#                        "animal litter", "vole poop",
-#                         "Vole poop", "Unk?"))
+
+# Retaining only Arctic National Wildlife Refuge (ANWR) site 
+ANWR_veg <- itex.full11 %>%
+  filter(SITE=="ANWR")
+
+# Range of years 
+range(ANWR_veg$YEAR) 
+# 1997-2007
+
+### Shrub cover over time  ----
+(ggplot(ANWR_veg, aes(x = YEAR, y = ShrubCover))+
+  geom_point(size = 2) +
+  geom_smooth(method = "lm"))
+## Shrub cover increasing 
+
+lm_shrub <- lm(ShrubCover~YEAR, data = ANWR_veg)
+summary(lm_shrub)
+# F-statistic: 9.151 on 1 and 1118 DF,  p-value: 0.002543
+
+### Graminoid cover over time  ----
+(ggplot(ANWR_veg, aes(x = YEAR, y = GraminoidCover))+
+   geom_point(size = 2) +
+   geom_smooth(method = "lm"))
+## Graminoid cover decreasing
+
+lm_graminoid <- lm(GraminoidCover~YEAR, data = ANWR_veg)
+summary(lm_graminoid)
+# F-statistic:  4.06 on 1 and 1118 DF,  p-value: 0.04415
+
+### Forb cover over time  ----
+(ggplot(ANWR_veg, aes(x = YEAR, y = ForbCover))+
+   geom_point(size = 2) +
+   geom_smooth(method = "lm"))
+## Forb cover decreasing 
+
+lm_forb <- lm(ForbCover~YEAR, data = ANWR_veg)
+summary(lm_forb)
+# F-statistic: 4.148 on 1 and 1118 DF,  p-value: 0.04191
+
+## put them all in same graph ! See OBAN project
+
+
 
