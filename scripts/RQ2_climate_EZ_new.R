d71e731aaaae0caacf4b63439f80b5dbdd68faac
Erica Zaja <ericazaja@gmail.com>
2022-03-03 17:26:15 +0000

    E makes panel of latlong temp /precip

--- scripts/RQ2_climate_EZ_new.R
+++ scripts/RQ2_climate_EZ_new.R
@@ -344,52 +344,84 @@ ggsave(file = "output/figures/biomass_vs_temp.png")
 # Checking temp ~ lat -----
 temp_lat_model <- lm(CH_TempMeanSummer~ latitude, data=coord.chelsa.combo.c)
 summary(temp_lat_model) 
-#F-statistic:  2621 on 1 and 19990 DF,  p-value: < 2.2e-16***
+#F-statistic:  1146 on 1 and 9573 DF,  p-value: < 2.2e-16***
 # temp decreases with latitude. I.e. northern latitudes colder
 
 (scatter_lat_temp <- ggplot(coord.chelsa.combo.c, aes(x =latitude , y = CH_TempMeanSummer )) +
-   geom_point(size = 0.1) +
-   geom_smooth(method = "lm") +
-   theme_classic())
+   geom_point(size = 0.3, color="skyblue") +
+   geom_smooth(method = "lm", colour = "black") +
+    labs(x = "\nLatitude", y = "Mean summer temperature (°C)\n")+ 
+    annotate(geom = "text", x = 70, y = 11, label="(a)", size = 10) +
+    annotate(geom = "text", x = 69.9, y = 7, label="slope = -1.24338*** ", size = 6) +
+   theme_shrub())
+  #+ theme(axis.title.y =element_text(size=12), 
+                        axis.title.x = element_text(size=12)))
 
 ggsave(file = "output/figures/scatter_lat_temp.png")
 
 # Checking temp ~ long -----
-temp_long_model <- lm(CH_TempMeanSummer~ longitude, data=coord.chelsa.combo.c)
+temp_long_model <- lm(CH_TempMeanSummer ~ longitude, data=coord.chelsa.combo.c)
 summary(temp_long_model) 
-# longitude    0.061776   0.003212   19.23   <2e-16 ***
+#F-statistic: 172.3 on 1 and 9573 DF,  p-value: < 2.2e-16 ***
 # temp increases with longitude. 
 
 (scatter_long_temp <- ggplot(coord.chelsa.combo.c, aes(x =longitude , y = CH_TempMeanSummer )) +
-    geom_point(size = 0.1) +
-    geom_smooth(method = "lm") +
-    theme_classic())
+    geom_point(size = 0.3, color = "skyblue") +
+    geom_smooth(method = "lm", color="black") +
+    labs(x = "\nLongitude", y = "Mean summer temperature (°C)\n")+ 
+    annotate(geom = "text", x = -141, y = 11, label="(b)", size = 10) +
+    annotate(geom = "text", x = -142, y = 7, label="slope = 0.061870*** ", size = 6) +
+    theme_shrub())
+  #+ theme(axis.title.y =element_text(size=12), 
+                         #axis.title.x = element_text(size=12)))
 
 ggsave(file = "output/figures/scatter_long_temp.png")
 
 # Checking precip ~ lat -----
-precip_lat_model <- lm(CH_PrecipMeanSummer~ latitude, data=coord.chelsa.combo.c)
+precip_lat_model <- lm(CH_PrecipMeanSummer ~ latitude, data=coord.chelsa.combo.c)
 summary(precip_lat_model) 
-#F-statistic: 6.886e+04 on 1 and 19990 DF,  p-value: < 2.2e-16***
+#F-statistic: 3.321e+04 on 1 and 9573 DF,  p-value: < 2.2e-16***
 # precip decreases with latitude too
 
-(scatter_lat_precip<- ggplot(coord.chelsa.combo.c, aes(x =latitude , y = CH_PrecipMeanSummer )) +
-    geom_point(size = 0.1) +
-    geom_smooth(method = "lm") +
-    theme_classic())
+(scatter_lat_precip <- ggplot(coord.chelsa.combo.c, aes(x =latitude , y = CH_PrecipMeanSummer )) +
+    geom_point(size = 0.3, color = "skyblue") +
+    geom_smooth(method = "lm", color="black") +
+    labs(x = "\nLatitude", y = "Mean summer precipitation (kg/m2)\n")+ 
+    annotate(geom = "text", x = 70, y = 125, label="(c)", size = 10) +
+    annotate(geom = "text", x = 69.9, y = 100, label="slope = -65.5282*** ", size = 6) +
+    theme_shrub() )
+  #+ theme(axis.title.y =element_text(size=12), 
+          #axis.title.x = element_text(size=12)))
 
-#ggsave(file = "output/figures/scatter_lat_precip.png")
+ggsave(file = "output/figures/scatter_lat_precip.png")
 
 # Checking precip ~ long -----
 precip_long_model <- lm(CH_PrecipMeanSummer~ longitude, data=coord.chelsa.combo.c)
 summary(precip_long_model) 
-# F-statistic: 452.8 on 1 and 19990 DF,  p-value: < 2.2e-16
+# F-statistic: 214.5 on 1 and 9573 DF,  p-value: < 2.2e-16***
 # precip increases wih long 
 
 (scatter_long_precip<- ggplot(coord.chelsa.combo.c, aes(x =longitude , y = CH_PrecipMeanSummer )) +
-    geom_point(size = 0.1) +
-    geom_smooth(method = "lm") +
-    theme_classic())
+    geom_point(size = 0.3, color = "skyblue") +
+    geom_smooth(method = "lm", color="black") +
+    labs(x = "\nLongitude", y = "Mean summer precipitation (kg/m2)\n")+ 
+    annotate(geom = "text", x = -141.5, y = 125, label="(d)", size = 10) +
+    annotate(geom = "text", x = -141.5, y = 70, label="slope = 1.34762*** ", size = 6) +
+    theme_shrub())
+  #+ theme(axis.title.y =element_text(size=12), 
+                         #axis.title.x = element_text(size=12)))
+    
+ggsave(file = "output/figures/scatter_long_precip.png")
+
+# Panel -----
+(panel_temp_precip_coords <- grid.arrange(arrangeGrob(scatter_lat_temp, scatter_long_temp,
+                                           scatter_lat_precip, scatter_long_precip,
+                                           ncol = 2))) # Sets number of panel columns
+
+ggsave(panel_temp_precip_coords, file="output/figures/panel_temp_precip_coords.png", height = 16, width = 15)
+
+#  top = panel_title  # Adding panel title
+
 
 
 # END -----
