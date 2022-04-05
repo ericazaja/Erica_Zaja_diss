d765280c11498b27974c1e47e24ffc88082dae9d
Erica Zaja <ericazaja@gmail.com>
2022-04-05 16:50:51 +0100

    E makes scripts folders

---
+++ scripts/main_scripts/NDVI_experiments.R
@@ -0,0 +1,13 @@
+##%######################################################%##
+#                                                          #
+####            NDVI TRENDS EXPERIMENTS -----            ###
+#               Erica Zaja - 22/10/2021                   ##
+#                                                         #
+##%######################################################%##
+
+## Need to figure out logan's package 
+
+## NB (from Myers-Smith et al. 2011)
+# long-term satellite records (i.e. AVHRR, MODIS or Landsat) 
+# covering timespans relevant to climate warming is coarse (250 m–8 km) 
+# compared to the spatial heterogeneity of shrub patches in tundra ecosystems (1–200 m)
